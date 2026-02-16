cbede6164257bc2caaad2bf5d956bf13930bb804
Harry_Otis <otienoharry876@gmail.com>
2026-02-16 16:49:15 +0300

    feat: Initialize Innovate Hub web application with core UI, signup, admin pages, and Firebase integration.

--- Charitize-1.0.0/js/firebase-config.js
+++ Charitize-1.0.0/js/firebase-config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
 
@@ -19,5 +19,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
+const googleProvider = new GoogleAuthProvider();
 
-export { auth, db, analytics };
+export { auth, db, analytics, googleProvider };
